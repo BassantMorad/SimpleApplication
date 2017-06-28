dbbd13c5ca975adac1b25579ffa57bf538453bc0
BassantMorad <Bassant_Morad20@hotmail.com>
2017-06-29 01:22:30 +0200

    First commit

---
+++ SimpleApplication/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleApplication.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Username { set; get; }
+        public string Email { set; get; }
+        public string Password { set; get; }
+        public string Mobile { set; get; }
+        public string Address { set; get; }
+    }
+}
